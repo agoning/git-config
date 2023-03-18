ac7aa1d3044edc40f9485b262523a09d7b4796cd
lyy <lyy@lyy.com>
2023-03-18 10:10:05 +0800

    second commit

--- src/main/java/com/lantu/GitTest.java
+++ src/main/java/com/lantu/GitTest.java
@@ -3,5 +3,6 @@ package com.lantu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("nice too meet you");
     }
 }
