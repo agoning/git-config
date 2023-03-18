54d138ab5bcf9cd8bf217b4013d066e5cd675c43
lyy <lyy@lyy.com>
2023-03-18 10:21:26 +0800

    master commit03

--- src/main/java/com/lantu/GitTest.java
+++ src/main/java/com/lantu/GitTest.java
@@ -3,7 +3,7 @@ package com.lantu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("nice too meet you");
+        System.out.println("nice too meet you master test");
         System.out.println("hello git --hot-fix");
 
     }
