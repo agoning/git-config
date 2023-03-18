0425927169bf9c6e92f425de42fb84d1dded9f26
lyy <lyy@lyy.com>
2023-03-18 10:16:37 +0800

    hot-fix commit01

--- src/main/java/com/lantu/GitTest.java
+++ src/main/java/com/lantu/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("nice too meet you");
+        System.out.println("hello git --hot-fix");
+
     }
 }
