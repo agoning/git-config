5b17201a90da15ba3c38be8db53b061f723ab985
agoning <100942218+agoning@users.noreply.github.com>
2023-03-18 10:47:29 +0800

    Update GitTest.java
    pull test

--- src/main/java/com/lantu/GitTest.java
+++ src/main/java/com/lantu/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git --hot-fix");
 
         System.out.println("push test");
+        System.out.println("pull test");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           test");
     }
 }
